4aebfe1acde57f39d7cf149cedd8647053f39da1
Rust-Hack <gleb.aleksandrovich.rust@gmail.com>
2026-01-20 05:04:02 +0300

    page about and footer site

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,47 @@
-import { Link } from "react-router";
-import logoWhite from "../../assets/logoWhite.svg";
+import { NavLink } from "react-router-dom";
+import logoWhite from "../../assets/logoBlack.svg";
+import styles from "./Header.module.css";
+import "@fontsource/mulish/400.css";
 
 function Header() {
   return (
-    <header>
+    <header className={styles.header}>
       <div>
         <img src={logoWhite} alt="Logo" />
       </div>
-      <div>
-        <Link to="/">Главная</Link>
-        <Link to="/catalog">Каталог</Link>
-        <Link to="/about">О компании</Link>
-        <Link to="/contacts">Контакты</Link>
+      <div className={styles.linksBlock}>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? `${styles.link} ${styles.active}` : styles.link
+          }
+        >
+          Главная
+        </NavLink>
+        <NavLink
+          to="/catalog"
+          className={({ isActive }) =>
+            isActive ? `${styles.link} ${styles.active}` : styles.link
+          }
+        >
+          Каталог
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? `${styles.link} ${styles.active}` : styles.link
+          }
+        >
+          О компании
+        </NavLink>
+        <NavLink
+          to="/contacts"
+          className={({ isActive }) =>
+            isActive ? `${styles.link} ${styles.active}` : styles.link
+          }
+        >
+          Контакты
+        </NavLink>
       </div>
     </header>
   );
